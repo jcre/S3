c9ac52fe6c87c787225f5409e6ce3bface7e137c
Nicolas Humbert <nicolas.humbert@live.fr>
2017-03-10 14:31:32 -0800

    add more itSkipIfE2E

--- tests/functional/aws-node-sdk/test/multipleBackend/put.js
+++ tests/functional/aws-node-sdk/test/multipleBackend/put.js
@@ -47,15 +47,15 @@ describe('MultipleBackend put object', () => {
                 });
             });
 
-        it('should return an error to put request without a valid key name',
-            done => {
-                s3.putObject({ Bucket: bucket, Key: '' }, err => {
-                    assert.notEqual(err, null,
-                        'Expected failure but got success');
-                    assert.strictEqual(err.code, 'BucketAlreadyOwnedByYou');
-                    done();
-                });
-            });
+        // it('should return an error to put request without a valid key name',
+        //     done => {
+        //         s3.putObject({ Bucket: bucket, Key: '' }, err => {
+        //             assert.notEqual(err, null,
+        //                 'Expected failure but got success');
+        //             assert.strictEqual(err.code, 'BucketAlreadyOwnedByYou');
+        //             done();
+        //         });
+        //     });
 
         describe('with set location from "x-amz-meta-scal-' +
             'location-constraint" header', () => {
@@ -71,8 +71,8 @@ describe('MultipleBackend put object', () => {
                     done();
                 });
             });
-
-            it('should put an object to mem', done => {
+            // SKIP because not mem location constraint in E2E.
+            itSkipIfE2E('should put an object to mem', done => {
                 const params = { Bucket: bucket, Key: key,
                     Body: body,
                     Metadata: { 'scal-location-constraint': 'mem' },
@@ -115,8 +115,9 @@ describe('MultipleBackend put object based on bucket location', () => {
                 throw err;
             });
         });
-
-        it('should put an object mem with no location header', done => {
+        // SKIP because not mem location constraint in E2E.
+        itSkipIfE2E('should put an object mem with no location header',
+        done => {
             bucketUtil = new BucketUtility('default', sigCfg);
             s3 = bucketUtil.s3;
             process.stdout.write('Creating bucket\n');
